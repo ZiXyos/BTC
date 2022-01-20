b0bd01ab0daa94fdcfed8cc7c7adb2f360f6554d
ZiXyos <killian.moudery@epitech.eu>
2022-01-20 17:56:01 +0100

    [Add]: 👷🏾‍♂️ Makefile

--- date_structures/linked_list/linked_list.c
+++ date_structures/linked_list/linked_list.c
@@ -1,7 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdbool.h>
+/**
+ * @file linked_list.c
+ * @author ZiXyos
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+**/
 
 #include "linked_list.h"
 
@@ -37,67 +43,6 @@ void list_dump(linked_list_t list) {
         printf("%s\n", node->data);
 }
 
-bool list_add_elem_at_front(linked_list_t *list, char *data) {
-
-    linked_list_t tmp = malloc(sizeof(linked_list_t));
-    if (!tmp) return false;
-
-    tmp->data = data;
-    tmp->next = (*list);
-    (*list) = tmp;
-
-    return true;
-}
-
-bool list_add_elem_at_back(linked_list_t *list, char *data) {
-
-    if(!data) return false;
-
-    linked_list_t tmp = malloc(sizeof(linked_list_t));
-    if (!tmp) return false;
-
-    tmp->data = data;
-    tmp->next = NULL;
-
-    if(!list) {
-
-        (*list) = tmp;
-        return true;
-    }
-
-    linked_list_t last = (*list);
-    while(last->next) last = (linked_list_t)last->next;
-
-    last->next = tmp;
-    return true;
-}
-
-bool list_add_elem_at_position(linked_list_t *list, char *data, uint32_t pos) {
-
-    if (!data) return false;
-
-    if (pos == 0) return list_add_elem_at_front(list, data);
-    if (pos > list_get_size(*list)) return false;
-
-    linked_list_t node = malloc(sizeof(linked_list_t));
-    if (!node) return false;
-
-    node->data = data;
-    node->next = NULL;
-
-    if (!list) {
-
-        (*list) = node;
-        return true;
-    }
-
-    linked_list_t tmp = (*list);
-    for (uint32_t i = 0; i != pos -1; i++) tmp = (linked_list_t)tmp->next;
-    node->next = tmp->next;
-    tmp->next = node;
-
-    return true;
-}
 
 int main(int ac,char *const *av) {
 
--- date_structures/linked_list/linked_list.h
+++ date_structures/linked_list/linked_list.h
@@ -1,6 +1,21 @@
+/**
+ * @file linked_list.h
+ * @author ZiXyos
+ * @brief
+ * @version 0.1
+ * @date 2022-01-20
+ *
+ * @copyright Copyright (c) 2022
+ *
+**/
 
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct linked_list_node {
 
@@ -8,6 +23,14 @@ typedef struct linked_list_node {
     struct linked_list_node *next;
 } linked_list_node_t;
 
-typedef linked_list_node_t *linked_list_t ;
+typedef linked_list_node_t *linked_list_t;
+
+bool list_isEmpty(linked_list_t list);
+uint32_t list_get_size(linked_list_t list);
+void list_dump(linked_list_t list);
+
+bool list_add_elem_at_front(linked_list_t *list, char *data);
+bool list_add_elem_at_back(linked_list_t *list, char *data);
+bool list_add_elem_at_position(linked_list_t *list, char *data, uint32_t pos);
 
 #endif /* LINKED_LIST_H */
